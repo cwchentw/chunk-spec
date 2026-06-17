d8453081a8ffca98e3851294d23ac171bb81bd1a
ByteBard <cwchen123@gmail.com>
2026-06-17 09:49:02 +0800

    Clean some code

--- lib/ChunkSpec/AST/AbstractWord.pm
+++ lib/ChunkSpec/AST/AbstractWord.pm
@@ -3,10 +3,12 @@ use parent 'ChunkSpec::AST';
 
 use v5.36;
 
+
 use constant {
     CATEGORY => 'category',
     FORM     => 'form',
 };
+
 
 sub new($class) {
     my $self = $class->SUPER::new();
--- lib/ChunkSpec/AST/Statement.pm
+++ lib/ChunkSpec/AST/Statement.pm
@@ -3,10 +3,12 @@ use parent 'Parse::AST';
 
 use v5.36;
 
+
 use constant {
     TYPE_COMMENT_STATEMENT       => 'comment_statement',
     TYPE_GRAMMAR_CHUNK_STATEMENT => 'grammar_chunk_statement',
 };
+
 
 sub new($class) {
     my $self = $class->SUPER::new();
